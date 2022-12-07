49c7cb91690062e49789e3f8996f50dbe43b316f
Craig Miller <craigcmiller@gmail.com>
2022-12-07 19:34:30 +0000

    Units and minor mods

--- src/CraigMiller.BlazorMap/CraigMiller.BlazorMap/Map.razor.cs
+++ src/CraigMiller.BlazorMap/CraigMiller.BlazorMap/Map.razor.cs
@@ -6,122 +6,119 @@ using Microsoft.AspNetCore.Components.Web;
 using CraigMiller.BlazorMap.Engine;
 using CraigMiller.BlazorMap.Layers;
 using CraigMiller.BlazorMap.Layers.Tiling;
-using System.Text.Json;
 
-namespace CraigMiller.BlazorMap
+namespace CraigMiller.BlazorMap;
+
+public partial class Map
 {
-    public partial class Map
+    SKGLView? _view;
+    readonly string _id = $"{nameof(Map).ToLower()}_{Guid.NewGuid().ToString().Replace("-", "")}";
+    readonly MapEngine _map;
+
+    public Map()
     {
-        SKGLView? _view;
-        readonly string _id = $"{nameof(Map).ToLower()}_{Guid.NewGuid().ToString().Replace("-", "")}";
-        readonly MapEngine _map;
-
-        public Map()
-        {
-            _map = new MapEngine();
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            base.OnInitialized();
-
-            await using MapJsInterop mapJsInterop = new(JSRuntime!);
-            await mapJsInterop.FitToContainer(_id);
-        }
-
-        public void AddDefaultLayers()
-        {
-            _map.AddLayer(new BackgroundFillLayer());
-
-            _map.AddLayer(new TileLayer(new HttpTileLoader(HttpClient!)));
-
-            _map.AddLayer(new GridLineLayer());
-        }
-
-        public void AddDebugLayers()
-        {
-            AddDefaultLayers();
-
-            _map.AddLayer(new CircleMarkerLayer
-            {
-                Locations = new List<Location> {
-                    new Location(51,0),
-                    new Location(80,-170),
-                    new Location(80,170),
-                    new Location(-80, -170),
-                    new Location(-80, 170)
-                }
-            });
-
-            _map.AddLayer(new DiagnosticsLayer());
-        }
-
-        public void AddMapLayer(ILayer layer) => _map.AddLayer(layer);
-
-        public async Task AddAsyncRenderMapLayer(params ILayer[] layers)
-        {
-            try
-            {
-                var asyncLayer = new AsynchronousLayer(layers);
-
-                AddMapLayer(asyncLayer);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-
-        public MapEngine Engine => _map;
-
-        private void OnPaintSurface(SKPaintGLSurfaceEventArgs paintEventArgs)
-        {
-            //Console.WriteLine($"WV: {paintEventArgs.BackendRenderTarget.Width}, {paintEventArgs.BackendRenderTarget.Height} - {paintEventArgs.Info.Width} {paintEventArgs.Info.Height}");
-
-            _map.AreaView.CanvasWidth = paintEventArgs.Info.Width;
-            _map.AreaView.CanvasHeight = paintEventArgs.Info.Height;
-
-            SKCanvas canvas = paintEventArgs.Surface.Canvas;
-            //canvas.Scale(paintEventArgs.BackendRenderTarget.Width / paintEventArgs.Info.Width, paintEventArgs.BackendRenderTarget.Height / paintEventArgs.Info.Height);
-            //canvas.Scale(paintEventArgs.Info.Width / paintEventArgs.BackendRenderTarget.Width, paintEventArgs.Info.Height / paintEventArgs.BackendRenderTarget.Height);
-
-            _map.Draw(canvas);
-        }
-
-        private void OnMouseDown(MouseEventArgs args)
-        {
-            switch (args.Button)
-            {
-                case 0:
-                    _map.PrimaryMouseDown(args.OffsetX, args.OffsetY);
-                    break;
-            }
-        }
-
-        private void OnMouseUp(MouseEventArgs args)
-        {
-            _map.PrimaryMouseUp();
-        }
-
-        private void OnMouseMove(MouseEventArgs args)
-        {
-            //Console.WriteLine($"Mouse: {args.OffsetX} {args.OffsetY}");
-
-            if (args.Button == 0)
-            {
-                _map.PrimaryMouseMove(args.OffsetX, args.OffsetY);
-            }
-        }
-
-        private void OnMouseWheel(WheelEventArgs args)
-        {
-            _map.ZoomOn(args.OffsetX, args.OffsetY, -args.DeltaY / 240d / 20d);
-        }
-
-        [Inject]
-        public HttpClient? HttpClient { get; set; }
-
-        [Inject]
-        public IJSRuntime? JSRuntime { get; set; }
+        _map = new MapEngine();
     }
+
+    protected override async Task OnInitializedAsync()
+    {
+        base.OnInitialized();
+
+        await using MapJsInterop mapJsInterop = new(JSRuntime!);
+        await mapJsInterop.FitToContainer(_id);
+    }
+
+    public void AddDefaultLayers()
+    {
+        _map.AddLayer(new BackgroundFillLayer());
+        _map.AddLayer(new TileLayer(new HttpTileLoader(HttpClient!)));
+        _map.AddLayer(new GridLineLayer());
+        _map.AddLayer(new ScaleLayer());
+    }
+
+    public void AddDebugLayers()
+    {
+        AddDefaultLayers();
+
+        _map.AddLayer(new CircleMarkerLayer
+        {
+            Locations = new List<Location> {
+                new Location(51,0),
+                new Location(80,-170),
+                new Location(80,170),
+                new Location(-80, -170),
+                new Location(-80, 170)
+            }
+        });
+
+        _map.AddLayer(new DiagnosticsLayer());
+    }
+
+    public void AddMapLayer(ILayer layer) => _map.AddLayer(layer);
+
+    public async Task AddAsyncRenderMapLayer(params ILayer[] layers)
+    {
+        try
+        {
+            var asyncLayer = new AsynchronousLayer(layers);
+
+            AddMapLayer(asyncLayer);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    public MapEngine Engine => _map;
+
+    private void OnPaintSurface(SKPaintGLSurfaceEventArgs paintEventArgs)
+    {
+        //Console.WriteLine($"WV: {paintEventArgs.BackendRenderTarget.Width}, {paintEventArgs.BackendRenderTarget.Height} - {paintEventArgs.Info.Width} {paintEventArgs.Info.Height}");
+
+        _map.AreaView.CanvasWidth = paintEventArgs.Info.Width;
+        _map.AreaView.CanvasHeight = paintEventArgs.Info.Height;
+
+        SKCanvas canvas = paintEventArgs.Surface.Canvas;
+        //canvas.Scale(paintEventArgs.BackendRenderTarget.Width / paintEventArgs.Info.Width, paintEventArgs.BackendRenderTarget.Height / paintEventArgs.Info.Height);
+        //canvas.Scale(paintEventArgs.Info.Width / paintEventArgs.BackendRenderTarget.Width, paintEventArgs.Info.Height / paintEventArgs.BackendRenderTarget.Height);
+
+        _map.Draw(canvas);
+    }
+
+    private void OnMouseDown(MouseEventArgs args)
+    {
+        switch (args.Button)
+        {
+            case 0:
+                _map.PrimaryMouseDown(args.OffsetX, args.OffsetY);
+                break;
+        }
+    }
+
+    private void OnMouseUp(MouseEventArgs args)
+    {
+        _map.PrimaryMouseUp();
+    }
+
+    private void OnMouseMove(MouseEventArgs args)
+    {
+        //Console.WriteLine($"Mouse: {args.OffsetX} {args.OffsetY}");
+
+        if (args.Button == 0)
+        {
+            _map.PrimaryMouseMove(args.OffsetX, args.OffsetY);
+        }
+    }
+
+    private void OnMouseWheel(WheelEventArgs args)
+    {
+        _map.ZoomOn(args.OffsetX, args.OffsetY, -args.DeltaY / 240d / 20d);
+    }
+
+    [Inject]
+    public HttpClient? HttpClient { get; set; }
+
+    [Inject]
+    public IJSRuntime? JSRuntime { get; set; }
 }
---
+++ src/CraigMiller.BlazorMap/CraigMiller.BlazorMap/Units/Pressure.cs
@@ -0,0 +1,64 @@
+﻿namespace CraigMiller.BlazorMap.Units;
+
+public readonly struct Pressure
+{
+    const double HectopascalInPascals = 100;
+    const double HectopascalInInchesOfMercury = 0.02953;
+    const double HectopascalInBars = 0.001;
+    const double HectopascalInPoundsPerSquareInch = 0.0145038;
+
+    public readonly double Hectopascals;
+
+    public static Pressure FromHectopascals(double hectopascals) => new Pressure(hectopascals, PressureUnits.Hectopascals);
+
+    public static Pressure FromPascals(double pascals) => new Pressure(pascals, PressureUnits.Pascals);
+
+    public static Pressure FromInchesOfMercury(double inchesOfMercury) => new Pressure(inchesOfMercury, PressureUnits.InchesOfMercury);
+
+    public static Pressure FromBars(double bars) => new Pressure(bars, PressureUnits.Bars);
+
+    public static Pressure FromPoundsPerSquareInch(double psi) => new Pressure(psi, PressureUnits.PoundsPerSquareInch);
+
+    public Pressure(double value, PressureUnits units)
+    {
+        Hectopascals = units switch
+        {
+            PressureUnits.Pascals => value / HectopascalInPascals,
+            PressureUnits.Hectopascals => value,
+            PressureUnits.InchesOfMercury => value / HectopascalInInchesOfMercury,
+            PressureUnits.Bars => value / HectopascalInBars,
+            PressureUnits.PoundsPerSquareInch => value / HectopascalInPoundsPerSquareInch,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    public double GetValue(PressureUnits units)
+    {
+        return units switch
+        {
+            PressureUnits.Pascals => Pascals,
+            PressureUnits.Hectopascals => Hectopascals,
+            PressureUnits.InchesOfMercury => InchesOfMercury,
+            PressureUnits.Bars => Bars,
+            PressureUnits.PoundsPerSquareInch => PoundsPerSquareInch,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    public double Pascals => Hectopascals * HectopascalInPascals;
+
+    public double InchesOfMercury => Hectopascals * HectopascalInInchesOfMercury;
+
+    public double Bars => Hectopascals * HectopascalInBars;
+
+    public double PoundsPerSquareInch => Hectopascals * HectopascalInPoundsPerSquareInch;
+}
+
+public enum PressureUnits
+{
+    Pascals,
+    Hectopascals,
+    InchesOfMercury,
+    Bars,
+    PoundsPerSquareInch
+}
